a32611fd0d2b5c91276d17072f4da75b6e8b617d
ziad-jradeh <ziad-jradeh@users.noreply.github.com>
2023-04-27 23:40:12 +0200

    update

--- client/main.py
+++ client/main.py
@@ -6,6 +6,19 @@ import requests
 BASE_URL = "localhost:8000"
 
 
+def format_game(fields):
+    for i, row in enumerate(fields):
+        for j, cell in enumerate(row):
+            if cell is None:
+                fields[i][j] = " "
+                
+    return f'''
+            {fields[0][0]} | {fields[0][1]} | {fields[0][2]}
+            {fields[1][0]} | {fields[1][1]} | {fields[1][2]}
+            {fields[2][0]} | {fields[2][1]} | {fields[2][2]}
+            '''
+    
+
 def check_response(response) -> bool:
     if response.status_code >= 400:
         print(f"Got response {response.status_code}: {response.text}")
@@ -23,6 +36,8 @@ def parse_args():
             "list-games",
             "create-game",
             "make-move",
+            "register-player",
+            "join-game",
         ],
         help="Action to perform",
     )
@@ -37,29 +52,45 @@ def list_games():
         return
 
     data = response.json()
-    print(json.dumps(data, indent=4))
+    
+    for game in data["games"]:
+        cells = format_game(game["field"])
+        del game["field"]
+        print(json.dumps(game, indent=4))
+        print(cells)
+        print("--------------------------------")
+        
+        
+        
 
 
 def create_game():
-    response = requests.post(f"http://{BASE_URL}/create-game/")
+    player_id = input("Player ID: ")
+    response = requests.post(f"http://{BASE_URL}/create-game/", json={"player_id": player_id})
     if not check_response(response):
         return
 
-    print(response.json())
+    game = response.json()
+    cells = format_game(game["field"])
+    del game["field"]
+    print(json.dumps(game, indent=4))
+    print(cells)
+    print("--------------------------------")
+    # print(response.json())
 
 
 def make_move():
-    game_id = int(input("Game ID: "))
+    game_id = input("Game ID: ")
     row = int(input("Row: "))
     col = int(input("Col: "))
-    player = input("Player: ")
+    player_id = input("Player ID: ")
 
     response = requests.post(
         f"http://{BASE_URL}/game/{game_id}/move/",
         json={
             "row": row,
             "col": col,
-            "player": player,
+            "player_id": player_id,
         },
     )
     if not check_response(response):
@@ -67,6 +98,35 @@ def make_move():
 
     print(response.json())
 
+
+def register_player():
+    name = input("Name: ")
+    response = requests.post(
+        f"http://{BASE_URL}/player/",
+        json={
+            "name": name,
+        },
+    )
+    if not check_response(response):
+        return
+
+    print(response.json())
+
+
+def join_game():
+    game_id = input("Game ID: ")
+    player_id = input("Player ID: ")
+    response = requests.post(
+        f"http://{BASE_URL}/join-game/",
+        json={
+            "game_id": game_id,
+            "player_id": player_id
+        },
+    )
+    if not check_response(response):
+        return
+
+    print(response.json())
 
 def main():
     args = parse_args()
@@ -77,6 +137,10 @@ def main():
         create_game()
     elif args.action == "make-move":
         make_move()
+    elif args.action == "register-player":
+        register_player()
+    elif args.action == "join-game":
+        join_game()
     else:
         raise ValueError(f"Unknown action: {args.action}")
 
--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 from fastapi import HTTPException
 
@@ -16,9 +17,8 @@ async def list_games():
 
 
 @app.post("/create-game/")
-async def create_game():
-    return storage.create_game()
-
+async def create_game(data: dict):
+    return storage.create_game(data["player_id"])
 
 def check_winner(game: dict) -> str | None:
     """
@@ -61,40 +61,84 @@ def check_winner(game: dict) -> str | None:
         game["status"] = "finished"
         return field[0][2]
 
+    draw_flag = True
+    for row in field:
+        for cell in row:
+            if cell is None:
+                draw_flag = False
+    if draw_flag:
+        game["status"] = "finished"
+        return "draw"
+    
     return None
 
 
 @app.post("/game/{game_id}/move/")
-async def make_move(game_id: int, move: schemas.Move):
+async def make_move(game_id: str, move: schemas.Move):
     game = storage.get_game(game_id)
-
+    
     if game is None:
         raise HTTPException(status_code=404, detail="Game not found")
 
-    if game["status"] != "new":
-        raise HTTPException(status_code=400, detail="Game is not new")
+    # if game["status"] != "new":
+    #     raise HTTPException(status_code=400, detail="Game is not new")
 
     if game["field"][move.row][move.col] is not None:
         raise HTTPException(status_code=400, detail="Cell is not empty")
 
-    if game["current_move"] != move.player:
+    if game["last_move_player_id"] == move.player_id:
         raise HTTPException(status_code=400, detail="Wrong player")
 
-    game["current_move"] = "x" if move.player == "o" else "o"
-    game["field"][move.row][move.col] = move.player
+    game["last_move_player_id"] = game["player_x"] if move.player_id == game["player_x"] else game["player_o"]
+    if move.player_id == game["player_x"]:
+        game["field"][move.row][move.col] = "x"
+    elif move.player_id == game["player_o"]:
+        game["field"][move.row][move.col] = "o"
+    else:
+        raise HTTPException(status_code=400, detail="Player not in this game.")
+    
+    
 
     winner = check_winner(game)
 
     storage.update_game(game_id, game)
 
     resp = {"status": "ok"}
-    if winner is not None:
-        resp["winner"] = winner
+    if winner == "x":
+        resp["winner"] = "player_x"
+        storage.update_wins(game["player_x"], 1)
+    elif winner == "o":
+        resp["winner"] = "player_o"
+        storage.update_wins(game["player_o"], 1)
+    elif winner == "draw":
+        resp["winner"] = "draw"
+        storage.update_wins(game["player_x"], 0.5)
+        storage.update_wins(game["player_o"], 0.5)
 
     return resp
 
 
+@app.post("/player/")
+def register_player(player: schemas.Player):
+    storage.register_player(player)
+    return player
+
+
+@app.post("/join-game/")
+def join_game(jg: schemas.JoinGame):
+    game = storage.get_game(jg.game_id)
+    if game is None:
+        raise ValueError(f"No game with id {jg.game_id}")
+    if game["status"] != "new":
+        raise ValueError(f"Game is not new")
+
+    if game["player_x"] != jg.player_id:
+        game["player_o"] = jg.player_id
+        game["status"] = "started"
+        storage.update_game(jg.game_id, game)
+        return game
+
+
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
